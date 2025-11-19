9c50b93c80d18874fb900901434c8de2c1e54502
Filip Szwerluga <filip.szwerluga@kainos.com>
2025-11-19 16:59:02 +0000

    Fix Terraform formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -26,8 +26,8 @@ resource "azurerm_key_vault" "kv" {
 
 # Container App Environment
 resource "azurerm_container_app_environment" "env" {
-  name                       = var.container_app_environment_name
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  tags                       = var.tags
+  name                = var.container_app_environment_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
 }
