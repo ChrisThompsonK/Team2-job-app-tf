3781e24a6df7c6c38cf79988693623c4933ae878
Filip Szwerluga <filip.szwerluga@kainos.com>
2025-11-19 16:50:59 +0000

    Key Vaults

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,4 +1,33 @@
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
+  tags     = var.tags
+}
+
+# Data source to get current user info
+data "azurerm_client_config" "current" {}
+
+# Azure Key Vault
+resource "azurerm_key_vault" "kv" {
+  name                       = var.key_vault_name
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
+
+  # Enable RBAC authorization instead of access policies
+  enable_rbac_authorization = true
+
+
+  tags = var.tags
+}
+
+# Container App Environment
+resource "azurerm_container_app_environment" "env" {
+  name                       = var.container_app_environment_name
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tags                       = var.tags
 }
