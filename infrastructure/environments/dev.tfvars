737005c0931f7a1c8249eaea465bff11e619119b
FilipKainos <filip.szwerluga@kainos.com>
2025-11-19 16:30:34 +0000

    Pipeline to deploy Terraform infrastructure to Azure (#2)
    * Pipeline to deploy Terraform infrastructure to Azure
    
    * Pipeline Stuff
    
    * V1 -> V2
    
    * Dev Vars remove form gitignore
    
    * commit 1
    
    * Commit1.1
    
    * HCL to gitignore
    
    * HCL to gitignore

---
+++ infrastructure/environments/dev.tfvars
@@ -0,0 +1,5 @@
+resource_group_name            = "team2-job-app-shared-rg"
+location                       = "UK South"
+key_vault_name                 = "team2-job-app-keyvault"
+container_app_environment_name = "team2-job-app-container-app-environment"
+tags                           = {}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,26 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group to create"
+  default     = "team2-fs-test-rg-dev-rg"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure location for resources"
+  default     = "UK South"
+}
+variable "key_vault_name" {
+  type        = string
+  description = "Name of the Azure Key Vault"
+}
+
+variable "container_app_environment_name" {
+  type        = string
+  description = "Name of the Container App Environment"
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "Tags to apply to resources"
+  default     = {}
+}
