e09e3fe100adfc362cd988d826d1be2552ff253b
ryanmagee-kainos <ryan.magee@kainos.com>
2025-11-20 11:21:02 +0000

    Add Azure Key Vault and Container App Environment resources

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,26 +6,3 @@ resource "azurerm_resource_group" "rg" {
 
 # Data source to get current user info
 data "azurerm_client_config" "current" {}
-
-# Azure Key Vault
-resource "azurerm_key_vault" "kv" {
-  name                     = var.key_vault_name
-  location                 = azurerm_resource_group.rg.location
-  resource_group_name      = azurerm_resource_group.rg.name
-  tenant_id                = data.azurerm_client_config.current.tenant_id
-  sku_name                 = "standard"
-  purge_protection_enabled = true
-
-  # Enable RBAC authorization instead of access policies
-  enable_rbac_authorization = true
-
-  tags = var.tags
-}
-
-# Container App Environment
-resource "azurerm_container_app_environment" "env" {
-  name                = var.container_app_environment_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tags                = var.tags
-}
