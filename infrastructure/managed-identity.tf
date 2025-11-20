2fa7d8ec852400d96a81ca1c607732ec4cdc2b6d
ryanmagee-kainos <ryan.magee@kainos.com>
2025-11-20 12:07:33 +0000

    Update managed-identity.tf

--- infrastructure/managed-identity.tf
+++ infrastructure/managed-identity.tf
@@ -5,10 +5,3 @@ resource "azurerm_user_assigned_identity" "frontend" {
   resource_group_name = azurerm_resource_group.rg.name
   tags                = var.tags
 }
-
-# Allow managed identity to read secrets from Key Vault
-resource "azurerm_role_assignment" "managed_identity_kv_secrets" {
-  scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_user_assigned_identity.frontend.principal_id
-}
