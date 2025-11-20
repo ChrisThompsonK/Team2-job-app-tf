6f6c646b377ea0f1e85e4125bae51d43b0cdd405
ryanmagee-kainos <ryan.magee@kainos.com>
2025-11-20 10:27:19 +0000

    refactor: move Key Vault to separate key-vaults.tf file
    - Create dedicated key-vaults.tf for Key Vault resource
    - Keep main.tf focused on resource group and container app environment
    - Improve code organization following separation of concerns principle

---
+++ infrastructure/key-vaults.tf
@@ -0,0 +1,14 @@
+# Azure Key Vault
+resource "azurerm_key_vault" "kv" {
+  name                     = var.key_vault_name
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  sku_name                 = "standard"
+  purge_protection_enabled = true
+
+  # Enable RBAC authorization instead of access policies
+  enable_rbac_authorization = true
+
+  tags = var.tags
+}
